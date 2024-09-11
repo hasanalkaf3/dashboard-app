79a34732faaa7158bf23f70e7aec24b75ab3080e
hasanalkaf3 <hakalkaf482@gmail.com>
2024-09-11 14:45:19 +0300

    add SpeedInsights package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 
@@ -9,7 +11,10 @@ export default function RootLayout({
   return (
     <html lang="en" dir="ltr">
       {/* See https://tailwindcss.com/docs/dark-mode */}
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
